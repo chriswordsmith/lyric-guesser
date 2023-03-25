b8f8eeb2970e00de6b3cf4dd8da2c8b12e5bf697
Callum Quirk <callumbquirk@gmail.com>
2023-03-25 09:39:24 +0000

    comitting code to display rhyme api call, this needs to be updated at later stage so the word used to search is the one removed from the quote so can be given as hint

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { useState, useEffect } from 'react';
 import { getQuotes } from './services/quotes-fetch'
 import { getRoles } from '@testing-library/react';
 import DisplayQuote from './components/Quote-Display/displayquote';
+import DisplayRhyme from './components/Rhyme-Display/displayrhyme';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <header className="App-header">
       <main>
         <DisplayQuote />
+        <DisplayRhyme />
       </main>
       </header>
     </div>
--- src/components/Rhyme-Display/displayrhyme.js
+++ src/components/Rhyme-Display/displayrhyme.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { getQuotes } from '../../services/quotes-fetch';
+import { getRhyme } from '../../services/rhyme-fetch';
 
 
 
-const DisplayQuote = () => { 
-    const [quoteResponse, setResponse] = useState();
+const DisplayRhyme = () => { 
+    const [rhymeResponse, setResponse] = useState();
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-     fetchQuotes()
+     fetchRhyme()
       }, [])
    
 
-    const fetchQuotes = async () => {
+    const fetchRhyme = async () => {
     try{
-        const responses = await getQuotes()
+        const responses = await getRhyme()
         console.log(responses)
         setLoading(false)
         setResponse(responses.data)
@@ -31,12 +31,10 @@ const DisplayQuote = () => {
       <header>
       <h1>API CALL</h1>
       <p>{loading? "loading" : "done"}</p>
-       {!loading && <h2>{JSON.stringify(quoteResponse[0].quote) }</h2>}
-       
-       
+       {!loading && <h2>{JSON.stringify(rhymeResponse[0]) }</h2>}
       </header>
     </div>
   );
 }
 
-export default DisplayQuote
+export default DisplayRhyme
